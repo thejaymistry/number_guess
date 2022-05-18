600441ef905e9b6408a4b02dd673d15be46b673d
thejaymistry <thejaymistry@gmail.com>
2022-05-18 15:33:31 +0530

    guessed by user

--- guessUser.py
+++ guessUser.py
@@ -1,1 +1,21 @@
-print("Hello")
+import random
+
+def Computer_guess(x):
+    low=1
+    high=x
+    feedback=''
+
+    while feedback != 'c':
+        if(low!=high):
+          guess= random.randint(low,high)
+        else:
+            guess=low
+        feedback =input(f'is {guess} too high (H),too low (L), or correct (c) ??')
+        if feedback=='h' or feedback =='H':
+            high=guess-1
+        elif feedback=='l' or feedback =='L':
+            low=guess+1
+
+    print(f'Hurray ! you guessed it right ..the number is {guess}');
+
+Computer_guess(10)
